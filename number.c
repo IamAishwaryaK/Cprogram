405bfaf65d477dda8efaa6b24dcc9da9b9fa8f96
IamAishwaryaK <46197625+IamAishwaryaK@users.noreply.github.com>
2019-02-08 18:28:19 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter number:");
+    scanf("%d",&a);
+    if(a<0){
+        printf("Negative");
+    }
+    else if(a>0)
+{
+    printf("positive");
+    
+}   
+else{
+    printf("Zero");
+}
+    return 0;
+    }
